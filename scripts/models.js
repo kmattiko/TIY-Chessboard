77d15e4fd3bdd51c6c068b6c40bb6ffeea4ae3aa
Kim Mattiko <amberlie@rocketmail.com>
2015-05-26 22:26:06 -0400

    updating with an attempt at chess

--- scripts/models.js
+++ scripts/models.js
@@ -1,1 +1,24 @@
-just setting up for now...
+(function(window){
+
+var initBoard = [
+    ['R','N','B','Q','K','B','N','R'],
+    ['P','P','P','P','P','P','P','P'],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p','p','p','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
+
+    function move(board.join('\n') + '\n\n') {
+      console.log(board.join('\n'));
+    }
+
+  function moves([]){
+
+    }
+
+)}(window);
+
+board[1][1] = board[2][1];
+board[2][1] = ' ';
